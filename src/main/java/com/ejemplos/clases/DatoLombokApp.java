8a0c9e3f395d2ffac40b9d28119dd6682562ef9e
David Vaquero <pepesan@gmail.com>
2023-03-16 19:29:32 +0100

    mejora de  ejemplos de lombok

--- src/main/java/com/ejemplos/clases/DatoLombokApp.java
+++ src/main/java/com/ejemplos/clases/DatoLombokApp.java
@@ -4,7 +4,9 @@ public class DatoLombokApp {
     public static void main(String[] args) {
         DatoLombok datoLombok = new DatoLombok();
         datoLombok.setAge(2);
+        System.out.println(datoLombok.getAge());
         datoLombok.setName("David");
+        System.out.println(datoLombok.getName());
         System.out.println(datoLombok);
         datoLombok = new DatoLombok("David", 2);
         System.out.println(datoLombok);
