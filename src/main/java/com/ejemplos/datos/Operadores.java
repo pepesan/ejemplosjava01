a2a2d69ca5b087bc312991d21f476304dcea5b7b
pepesan <pepesan@gmail.com>
2024-10-08 16:04:25 +0200

    mejora del ejemplo de operadores

--- src/main/java/com/ejemplos/datos/Operadores.java
+++ src/main/java/com/ejemplos/datos/Operadores.java
@@ -6,7 +6,7 @@ public class Operadores {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Ejemplos de operadores
 		int i,j,k;
 		double d;
 		i=7;
@@ -17,12 +17,14 @@ public class Operadores {
 		System.out.println(k);
 		k=i*j;
 		System.out.println(k);
-		d=i/j;
+		d= (double) i /j;
 		System.out.println(d);
 		d=i%j;
 		System.out.println(d);
+		// preincremento
 		++i;
 		// i = i +1;
+		// postincremento
 		i++;
 		System.out.println(i);
 		i= 1;
@@ -42,7 +44,11 @@ public class Operadores {
 		// j = 3;
 		// i = 2;
 		System.out.println(j);
+		// postdecremento
 		i--;
+		System.out.println(i);
+		// predecremento
+		--i;
 		System.out.println(i);
 		//Operador condicional
 		k=4;
@@ -53,6 +59,7 @@ public class Operadores {
 		// i = i +j; // ==5
 		i+=j;
 		System.out.println(i);
+		// i = i - j;
 		i-=j;
 		System.out.println(i);
 		i*=j;
